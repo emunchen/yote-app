af3fe0e78660fc6f498de89e2e57173c4602f12a
Emanuel Pécora <emanuelmza@gmail.com>
2020-05-05 20:20:50 -0300

    add logic after note creation.

--- web/resources/note/views/CreateNote.js.jsx
+++ web/resources/note/views/CreateNote.js.jsx
@@ -5,41 +5,38 @@
  */
 
 // import primary libraries
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { history, withRouter } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { history, withRouter } from "react-router-dom";
 
 // import third-party libraries
-import _ from 'lodash';
+import _ from "lodash";
 
 // import actions
-import * as noteActions from '../noteActions';
+import * as noteActions from "../noteActions";
 
 // import global components
-import Binder from '../../../global/components/Binder.js.jsx';
+import Binder from "../../../global/components/Binder.js.jsx";
 
 // import resource components
-import NoteForm from '../components/NoteForm.js.jsx';
-import NoteLayout from '../components/NoteLayout.js.jsx';
+import NoteForm from "../components/NoteForm.js.jsx";
+import NoteLayout from "../components/NoteLayout.js.jsx";
 
 class CreateNote extends Binder {
   constructor(props) {
     super(props);
     this.state = {
-      note: _.cloneDeep(this.props.defaultNote.obj)
+      note: _.cloneDeep(this.props.defaultNote.obj),
       // NOTE: We don't want to actually change the store's defaultItem, just use a copy
-      , formHelpers: {}
+      formHelpers: {},
       /**
        * NOTE: formHelpers are useful for things like radio controls and other
        * things that manipulate the form, but don't directly effect the state of
        * the note
        */
-    }
-    this._bind(
-      '_handleFormChange'
-      , '_handleFormSubmit'
-    );
+    };
+    this._bind("_handleFormChange", "_handleFormSubmit");
   }
 
   componentDidMount() {
@@ -49,9 +46,8 @@ class CreateNote extends Binder {
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      note: _.cloneDeep(nextProps.defaultNote.obj)
-
-    })
+      note: _.cloneDeep(nextProps.defaultNote.obj),
+    });
   }
   _handleFormChange(e) {
     /**
@@ -60,17 +56,19 @@ class CreateNote extends Binder {
     let newState = _.update(this.state, e.target.name, () => {
       return e.target.value;
     });
-    this.setState({newState});
+    this.setState({ newState });
   }
 
-
   _handleFormSubmit(e) {
-    const { dispatch, history } = this.props;
+    const { dispatch, history, currentUser, match } = this.props;
+    let newNote = { ...this.state.note };
+    newNote._task = match.params.taskId;
+    newNote._user = currentUser._id;
     e.preventDefault();
-    dispatch(noteActions.sendCreateNote(this.state.note)).then(noteRes => {
-      if(noteRes.success) {
+    dispatch(noteActions.sendCreateNote(newNote)).then((noteRes) => {
+      if (noteRes.success) {
         dispatch(noteActions.invalidateList());
-        history.push(`/notes/${noteRes.item._id}`)
+        history.push(`/tasks/${newNote._task}`);
       } else {
         alert("ERROR - Check logs");
       }
@@ -79,12 +77,13 @@ class CreateNote extends Binder {
 
   render() {
     const { note, formHelpers } = this.state;
-    const isEmpty = (!note || note.name === null || note.name === undefined);
+    const isEmpty =
+      !note || note.content === null || note.content === undefined;
     return (
       <NoteLayout>
-        { isEmpty ?
+        {isEmpty ? (
           <h2> Loading...</h2>
-          :
+        ) : (
           <NoteForm
             note={note}
             cancelLink="/notes"
@@ -93,16 +92,16 @@ class CreateNote extends Binder {
             formType="create"
             handleFormChange={this._handleFormChange}
             handleFormSubmit={this._handleFormSubmit}
-            />
-        }
+          />
+        )}
       </NoteLayout>
-    )
+    );
   }
 }
 
 CreateNote.propTypes = {
-  dispatch: PropTypes.func.isRequired
-}
+  dispatch: PropTypes.func.isRequired,
+};
 
 const mapStoreToProps = (store) => {
   /**
@@ -111,14 +110,11 @@ const mapStoreToProps = (store) => {
    */
 
   // manipulate store items here
-
   return {
-    defaultNote: store.note.defaultItem
-  }
-}
+    //currentTask: store.task,
+    currentUser: store.user.loggedIn.user,
+    defaultNote: store.note.defaultItem,
+  };
+};
 
-export default withRouter(
-  connect(
-    mapStoreToProps
-  )(CreateNote)
-);
+export default withRouter(connect(mapStoreToProps)(CreateNote));
